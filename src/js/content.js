8fc82c005c968d5c5e3518b2c48faade121e92db
Sarah German <sgerman@smm.org>
2013-07-16 14:46:30 -0500

    Better data formatting

--- src/js/content.js
+++ src/js/content.js
@@ -3,49 +3,49 @@ var content = {
     {
       title : "Contemporary Pueblo people",
       step_2_title : "What can this modern Pueblo dancer tell us about her ancestors?",
-      prompts : {
-        question : "How are contemporary Pueblo people connected to this region and the ancestral Pueblo people?",
-        question : "Do modern Pueblo people live like their ancestors did?",
-        question : "What do contemporary Pueblo people know about where the ancestral Pueblo people went?",
-        question : "What do contemporary people do to preserve knowledge of the past?",
-        question : "Why are ancestral Pueblo sites important to contemporary Pueblo people?"
-      },
+      questions : [
+        "How are contemporary Pueblo people connected to this region and the ancestral Pueblo people?",
+        "Do modern Pueblo people live like their ancestors did?",
+        "What do contemporary Pueblo people know about where the ancestral Pueblo people went?",
+        "What do contemporary people do to preserve knowledge of the past?",
+        "Why are ancestral Pueblo sites important to contemporary Pueblo people?"
+      ],
       machine_name : "cpp"
     },
     {
       title : "Corn",
       step_2_title : "What can corn tell us about ancestral Pueblo people?",
-      prompts : {
-        question : "Why is corn an important part of the history of the Mesa Verde region?",
-        question : "Why is corn farming important to Pueblo people past and present?",
-        question : "When did they start growing corn?",
-        question : "How did they farm in this dry region?",
-        question : "Did the ancestral Pueblo people stop hunting after they could grow corn?"
-      },
+      questions : [
+        "Why is corn an important part of the history of the Mesa Verde region?",
+        "Why is corn farming important to Pueblo people past and present?",
+        "When did they start growing corn?",
+        "How did they farm in this dry region?",
+        "Did the ancestral Pueblo people stop hunting after they could grow corn?"
+      ],
       machine_name : "corn"
     },
     {
       title : "Pueblo dwellings",
       step_2_title : "What can Pueblo dwellings tell us about the people who lived there?",
-      prompts : {
-        question : "Did everyone live in the cliffs?",
-        question : "Why did people move to the cliffs?",
-        question : "Why did people live in different settlements at different times?",
-        question : "Did the ancestral Pueblo people live in cities?",
-        question : "Why are these sites special to modern Pueblo people?"
-      },
+      questions : [
+        "Did everyone live in the cliffs?",
+        "Why did people move to the cliffs?",
+        "Why did people live in different settlements at different times?",
+        "Did the ancestral Pueblo people live in cities?",
+        "Why are these sites special to modern Pueblo people?"
+      ],
       machine_name : "pdwells"
     },
     {
       title : "Mesa Verde region",
       step_2_title : "What can the Mesa Verde region tell us about ancestral Pueblo people?",
-      prompts : {
-        question : "Where did the ancestral Pueblo people go?",
-        question : "Why did the ancestral Pueblo people move away?",
-        question : "How is the Mesa Verde region bigger than just the national park?",
-        question : "How many people lived in the Mesa Verde region?",
-        question : "When and why did the ancestral Pueblo people choose this place to live?"
-      },
+      questions : [
+        "Where did the ancestral Pueblo people go?",
+        "Why did the ancestral Pueblo people move away?",
+        "How is the Mesa Verde region bigger than just the national park?",
+        "How many people lived in the Mesa Verde region?",
+        "When and why did the ancestral Pueblo people choose this place to live?"
+      ],
       machine_name : "mvreg"
     }
   ]
