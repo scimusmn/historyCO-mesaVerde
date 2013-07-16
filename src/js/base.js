23339a4f87f917ca944d13427825ae65c63463f6
Sarah German <sgerman@smm.org>
2013-07-16 14:46:41 -0500

    Hide/show action for step 1 buttons

--- src/js/base.js
+++ src/js/base.js
@@ -4,11 +4,24 @@
 
 $(function() {
 
-  // Get our template and turn it into HTML
+  // Get our step 1 template and turn it into HTML
   var source   = $('#template-1').html();
   var template = Handlebars.compile(source);
 
   // Pull in the content object from content.js
   $('#step-1-output').html(template(content));
+
+  // Samesies for step 2 template (TODO - loop these)
+  var step_2_source = $('#template-2').html();
+  var step_2_template = Handlebars.compile(step_2_source);
+  $('#step-2-output').html(step_2_template(content));
+
+  // Navigation over to step 2
+  $('div.section').click(function() {
+    var selected = $(this).attr('data-option-id');
+    $('#step-1-wrap').fadeOut('slow', function() {
+      $('#step-2-wrap').fadeIn('fast');
+    });
+  });
 
 });
